a4803cd5963f38101a5b8c347417ff2c6a8a15c2
Michael Boyles <michael@boyl.es>
2022-08-16 20:10:24 +0100

    refactor: convert one component to a record to prove that it works

--- test/src/main/java/com/github/michaelboyles/simpledi/test/Engine.java
+++ test/src/main/java/com/github/michaelboyles/simpledi/test/Engine.java
@@ -3,10 +3,5 @@ package com.github.michaelboyles.simpledi.test;
 import javax.inject.Singleton;
 
 @Singleton
-public class Engine {
-    private final Turbocharger turbocharger;
-
-    public Engine(Turbocharger turbocharger) {
-        this.turbocharger = turbocharger;
-    }
+public record Engine(Turbocharger turbocharger) {
 }
