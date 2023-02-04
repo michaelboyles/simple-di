e9394042b7a345048d21930c3898f0e44e423f6c
Michael Boyles <michael@boyl.es>
2023-02-04 16:22:42 +0000

    fix: missing class at runtime. Plugin is expected to have provided scope

--- runtime/src/main/java/com/github/michaelboyles/simpledi/MutableProvider.java
+++ runtime/src/main/java/com/github/michaelboyles/simpledi/MutableProvider.java
@@ -2,9 +2,17 @@ package com.github.michaelboyles.simpledi;
 
 import javax.inject.Provider;
 
+/**
+ * A simple holder for a value which can be provided in place of a direct dependency. The indirection
+ * allows for the avoidance of circular dependencies. This class is only expected to be used by the
+ * generated DI context.
+ *
+ * @param <T> The type of the mutable object reference.
+ */
 public class MutableProvider<T> implements Provider<T> {
     private T value;
 
+    @SuppressWarnings("unused") // Called by generated code only
     public void set(T value) {
         if (value == null) throw new NullPointerException();
         if (this.value != null) throw new RuntimeException("Already set to " + value);
